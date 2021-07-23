0f408dc3284239f2166b85abe7cc72ea86a13d72
Pete Williams <hello@petewritescode.com>
2021-07-23 21:28:00 +0100

    Remove utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 export { getSudoku } from './utils/get-sudoku.util';
-export { arrayToString } from './utils/array-to-string.util';
-export { stringToArray } from './utils/string-to-array.util';
--- src/utils/validate/validate-sequence.util.ts
+++
@@ -1,7 +0,0 @@
-import { GRID_SIZE } from '../../constants/grid-size.constant';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const validateSequence = (sequence: any): boolean =>
-  typeof sequence === 'string' &&
-  sequence.length === GRID_SIZE ** 2 &&
-  sequence.split('').every((token) => '123456789-'.includes(token));
